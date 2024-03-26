2ace1e9b854ac1cbc497ffe1145e9b75090ec1c5
7sevnen7seven <83464611+7sevnen7seven@users.noreply.github.com>
2024-03-26 14:42:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
-class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
-        this.index = index;
-        this.previousHash = previousHash.toString();
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = hash.toString();
+// Import the Solana Web3.js library and the file system library
+const solanaWeb3 = require('@solana/web3.js');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Read the private key from environment variables
+const PRIVATE_KEY = process.env.PRIVATE_KEY.split(',').map(num => parseInt(num, 10));
+const sender = solanaWeb3.Keypair.fromSecretKey(new Uint8Array(PRIVATE_KEY));
+
+// Read transfer targets from the configuration file
+const config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+const transfers = config.transfers;
+
+// Create a new connection to the Solana mainnet
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'), 'confirmed');
+
+// Create and sign transactions
+async function signAndSendTransactions() {
+  let transaction = new solanaWeb3.Transaction();
+  for (const [index, transfer] of transfers.entries()) {
+    transaction.add(
+      solanaWeb3.SystemProgram.transfer({
+        fromPubkey: sender.publicKey,
+        toPubkey: new solanaWeb3.PublicKey(transfer.to),
+        lamports: transfer.amount
+      })
+    );
+
+    // If the transaction is too large or it's the last transfer, send the transaction
+    if (transaction.instructions.length === solanaWeb3.Transaction.MAX_INSTRUCTIONS_PER_TRANSACTION || index === transfers.length - 1) {
+      try {
+        // Sign and send the transaction
+        const signature = await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [sender]);
+        console.log(`Transaction successful, signature: ${signature}`);
+      } catch (error) {
+        console.error('Batch transfer failed:', error);
+      }
+      // Reset the transaction for the next batch
+      transaction = new solanaWeb3.Transaction();
     }
+  }
 }
 
-const crypto = require('crypto');
-
-class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-    }
-
-    createGenesisBlock() {
-        return new Block(0, "0", 1465154705, "Genesis Block", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = this.calculateHash(newBlock);
-        this.chain.push(newBlock);
-    }
-
-    calculateHash(block) {
-        return crypto.createHash('sha256').update(block.index + block.previousHash + block.timestamp + JSON.stringify(block.data)).digest('hex');
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== this.calculateHash(currentBlock)) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-module.exports = Blockchain;
+// Execute batch transfers
+signAndSendTransactions();
